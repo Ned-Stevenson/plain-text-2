2eed353e158faec59885ffe30c713548a8c70042
Ned Stevenson <stuntphish@gmail.com>
2026-03-28 20:52:54 +0000

    [1.0.0]
    Added sidebar support
    Ran code beautifier
    Upgraded to manifest v3

--- js/moving.js
+++ js/moving.js
@@ -1,80 +1,43 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
 
+    var order = localStorage.getItem('order');
+    order = order.split(',');
 
+    var moveActiveSwitchButton = function(i) {
 
+        var switchButton = document.querySelector('.' + order[i] + '-switch-button');
 
-var order = localStorage.getItem('order');
-order = order.split(',');
+        switchButton.addEventListener('mousedown', function(event) {
+            event.preventDefault();
+            // localStorage.setItem('activeNote', order[i]);
+            // localStorage.setItem('activeColor', setColors[i]);
 
+            // switchButton.style.position = 'absolute';
 
+            // var yCoordinates = (event.pageY + '');
+            // yCoordinates = Number.parseInt(yCoordinates);
 
+            // var body = document.querySelector('body');
 
-var moveActiveSwitchButton = function (i) {
+            body.addEventListener('mousemove', function(event) {
 
+                // var newYCoordinates = (event.pageY + '');
+                // newYCoordinates = Number.parseInt(newYCoordinates);
 
-    var switchButton = document.querySelector('.' + order[i] + '-switch-button');
+                // switchButton.style.top = newYCoordinates + 'px';
 
-
-    switchButton.addEventListener('mousedown', function (event) {
-        event.preventDefault();
-        // localStorage.setItem('activeNote', order[i]);
-        // localStorage.setItem('activeColor', setColors[i]);
-
-        // switchButton.style.position = 'absolute';
-
-        // var yCoordinates = (event.pageY + '');
-        // yCoordinates = Number.parseInt(yCoordinates);
-
-
-        // var body = document.querySelector('body');
-
-        body.addEventListener('mousemove', function (event) {
-
-        
-
-        // var newYCoordinates = (event.pageY + '');
-        // newYCoordinates = Number.parseInt(newYCoordinates);
-
-        // switchButton.style.top = newYCoordinates + 'px';
-
-
-
-
-
-
+            });
 
         });
 
-
-
-        
-
-
-    });
-
-
-
     };
 
-
-
     for (var i = 0; i < order.length; i++) {
-    moveActiveSwitchButton(i);
-    }  
+        moveActiveSwitchButton(i);
+    }
 
+    // var switchButton = document.querySelector('.switch-button');
 
-
-
-
-// var switchButton = document.querySelector('.switch-button');
-
-// console.log(switchButton);
-
-
-
-
-
-
+    // console.log(switchButton);
 
 });
-
--- js/options.js
+++ js/options.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
 
     // set winow size
-    
+
     var bodyMain = document.querySelector('body');
     var notePadSizeBody = localStorage.getItem('notepad-size');
     var activeButtonSetWindowSize = localStorage.getItem('notepad-size-key');
@@ -15,13 +15,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // notepad size options
 
-    var setNotepadSize = function (size) {
+    var setNotepadSize = function(size) {
         var bodyMain = document.querySelector('body');
         var smallSizeButton = document.querySelector('.small-size');
         var mediumSizeButton = document.querySelector('.medium-size');
         var largeSizeButton = document.querySelector('.large-size');
         var button = document.querySelector('.' + size + '-size');
-        button.addEventListener('click', function (event) {
+        button.addEventListener('click', function(event) {
             event.preventDefault();
             removeSelectedClass('notepad-size-block');
             button.classList.add('selected');
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function () {
         aciveButton.classList.add(defaultColorButton + '-fill');
         var randomButton = document.querySelector('.random');
     }
-    var newNoteSetColor = function (color) {
+    var newNoteSetColor = function(color) {
         var setColorBlock = document.querySelector('.set-color-block');
         var allButtons = setColorBlock.querySelectorAll('.button');
         var button = setColorBlock.querySelector('.' + color + '-font');
@@ -76,7 +76,7 @@ document.addEventListener('DOMContentLoaded', function () {
             'black',
             'white'
         ];
-        button.addEventListener('click', function (event) {
+        button.addEventListener('click', function(event) {
             event.preventDefault();
             for (var i = 0; i < allButtons.length; i++) {
                 allButtons[i].classList.remove('selected');
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 localStorage.setItem('defaultColor', color);
             }
         });
-        buttonRandom.addEventListener('click', function (event) {
+        buttonRandom.addEventListener('click', function(event) {
             event.preventDefault();
             for (var i = 0; i < allButtons.length; i++) {
                 allButtons[i].classList.remove('selected');
@@ -115,10 +115,10 @@ document.addEventListener('DOMContentLoaded', function () {
     var fontBlock = document.querySelector('.font-pref');
     var activeButtonFontSet = fontBlock.querySelector('.' + fontDefault);
     activeButtonFontSet.classList.add('selected');
-    var fontPref = function (font) {
+    var fontPref = function(font) {
         var fontBlock = document.querySelector('.font-pref');
         var fontSet = fontBlock.querySelector('.' + font);
-        fontSet.addEventListener('click', function (event) {
+        fontSet.addEventListener('click', function(event) {
             event.preventDefault();
             removeSelectedClass('font-pref');
             fontSet.classList.add('selected');
@@ -145,17 +145,16 @@ document.addEventListener('DOMContentLoaded', function () {
         lessFontSize.classList.add('silent');
     } else if (defaultFontSize == 30) {
         moreFontSize.classList.add('silent');
-    } else if (defaultFontSize == 30) {
-    }
+    } else if (defaultFontSize == 30) {}
     defaultFontSize = Number.parseInt(defaultFontSize);
     var fontSizeScreen = fontSizeBlock.querySelector('.font-size-screen');
     fontSizeScreen.setAttribute('data-value', defaultFontSize);
     if (defaultFontSize == 16) {
         fontSizeScreen.classList.add('white-font');
-    } 
+    }
 
     fontSizeBlockExample.style.fontSize = defaultFontSize + 'px';
-    lessFontSize.addEventListener('click', function (event) {
+    lessFontSize.addEventListener('click', function(event) {
         event.preventDefault();
         moreFontSize.classList.remove('silent');
         defaultFontSize = defaultFontSize - 2;
@@ -172,7 +171,7 @@ document.addEventListener('DOMContentLoaded', function () {
         localStorage.setItem('font-size', defaultFontSize);
         fontSizeScreen.setAttribute('data-value', defaultFontSize);
     });
-    moreFontSize.addEventListener('click', function (event) {
+    moreFontSize.addEventListener('click', function(event) {
         event.preventDefault();
         lessFontSize.classList.remove('silent');
         defaultFontSize = defaultFontSize + 2;
